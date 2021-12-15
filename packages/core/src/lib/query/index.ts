cd4ee6974de256c4c037546a0b20ea4406521d16
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-12-15 18:52:21 +0100

    chore: #6 exist, null filters

--- packages/core/src/lib/query/index.ts
+++ packages/core/src/lib/query/index.ts
@@ -4,6 +4,10 @@ export type Matcher<T> = (item: T) => boolean;
 export interface Query<T> {
   contains(value: string): Matcher<T>;
   eq(value: string): Matcher<T>;
+  lt(value: string): Matcher<T>;
+  gt(value: string): Matcher<T>;
+  exist(): Matcher<T>;
+  isNull(): Matcher<T>;
   not: Query<T>;
 }
 
@@ -18,13 +22,47 @@ export class ItemQuery implements Query<Item> {
   contains(value: string): Matcher<Item> {
     return (item: Item) => item[this.property].indexOf(value) >= 0;
   }
+
+  lt(value: string): Matcher<Item> {
+    return (item: Item) => item[this.property] < value;
+  }
+
+  gt(value: string): Matcher<Item> {
+    return (item: Item) => item[this.property] > value;
+  }
+
+  exist(): Matcher<Item> {
+    return (item: Item) =>
+      item[this.property] !== null && item[this.property] !== undefined;
+  }
+
+  isNull(): Matcher<Item> {
+    return (item: Item) => item[this.property] === null;
+  }
 }
 
 class NotQuery<T> implements Query<T> {
   constructor(public readonly not: Query<T>) {}
+  lt(value: string): Matcher<T> {
+    return (item: T) => !this.not.lt(value)(item);
+  }
+
+  gt(value: string): Matcher<T> {
+    return (item: T) => !this.not.gt(value)(item);
+  }
+
+  exist(): Matcher<T> {
+    return (item: T) => !this.not.exist()(item);
+  }
+
+  isNull(): Matcher<T> {
+    return (item: T) => !this.not.isNull()(item);
+  }
+
   contains(value: string): Matcher<T> {
     return (item: T) => !this.not.contains(value)(item);
   }
+
   eq(value: string): Matcher<T> {
     return (item: T) => !this.not.eq(value)(item);
   }
--- packages/core/test/filters.spec.ts
+++ packages/core/test/filters.spec.ts
@@ -69,32 +69,223 @@ describe('filters', () => {
     ).resolves.toMatchObject([first]);
   });
 
-  it('should filter by equality', async () => {
-    expect(repo.searchItems(item('kind').eq('feature'))).resolves.toMatchObject(
-      [third]
-    );
+  it('should filter item strictly less than value', async () => {
+    await expect(
+      repo.searchItems(item('title').lt('Third'))
+    ).resolves.toMatchObject([second, first]);
 
-    expect(repo.searchItems(item('kind').eq('issue'))).resolves.toMatchObject([
-      second,
-      first,
-    ]);
+    await expect(
+      repo.searchItems(item('title').lt('Second'))
+    ).resolves.toMatchObject([first]);
+
+    await expect(
+      repo.searchItems(item('title').lt('second'))
+    ).resolves.toMatchObject([third, second, first]);
+
+    await expect(
+      repo.searchItems(item('title').lt('Univers'))
+    ).resolves.toMatchObject([third, second, first]);
+
+    await expect(
+      repo.searchItems(item('title').lt('Sec'))
+    ).resolves.toMatchObject([first]);
+
+    await expect(
+      repo.searchItems(item('title').lt('Hello'))
+    ).resolves.toMatchObject([first]);
+  });
+
+  it('should filter item *not* strictly less than value', async () => {
+    await expect(
+      repo.searchItems(item('title').not.lt('Third'))
+    ).resolves.toMatchObject([third]);
+
+    await expect(
+      repo.searchItems(item('title').not.lt('Second'))
+    ).resolves.toMatchObject([third, second]);
+
+    await expect(
+      repo.searchItems(item('title').not.lt('second'))
+    ).resolves.toMatchObject([]);
+
+    await expect(
+      repo.searchItems(item('title').not.lt('Univers'))
+    ).resolves.toMatchObject([]);
+
+    await expect(
+      repo.searchItems(item('title').not.lt('Sec'))
+    ).resolves.toMatchObject([third, second]);
+
+    await expect(
+      repo.searchItems(item('title').not.lt('Hello'))
+    ).resolves.toMatchObject([third, second]);
+  });
+
+  it('should filter item strictly greater than value', async () => {
+    await expect(
+      repo.searchItems(item('title').gt('Third'))
+    ).resolves.toMatchObject([]);
+
+    await expect(
+      repo.searchItems(item('title').gt('Second'))
+    ).resolves.toMatchObject([third]);
+
+    await expect(
+      repo.searchItems(item('title').gt('second'))
+    ).resolves.toMatchObject([]);
+
+    await expect(
+      repo.searchItems(item('title').gt('Univers'))
+    ).resolves.toMatchObject([]);
+
+    await expect(
+      repo.searchItems(item('title').gt('Sec'))
+    ).resolves.toMatchObject([third, second]);
+
+    await expect(
+      repo.searchItems(item('title').gt('Hello'))
+    ).resolves.toMatchObject([third, second]);
+  });
+
+  it('should filter item *not* strictly greater than value', async () => {
+    await expect(
+      repo.searchItems(item('title').not.gt('Third'))
+    ).resolves.toMatchObject([third, second, first]);
+
+    await expect(
+      repo.searchItems(item('title').not.gt('Second'))
+    ).resolves.toMatchObject([second, first]);
+
+    await expect(
+      repo.searchItems(item('title').not.gt('second'))
+    ).resolves.toMatchObject([third, second, first]);
+
+    await expect(
+      repo.searchItems(item('title').not.gt('Univers'))
+    ).resolves.toMatchObject([third, second, first]);
+
+    await expect(
+      repo.searchItems(item('title').not.gt('Sec'))
+    ).resolves.toMatchObject([first]);
+
+    await expect(
+      repo.searchItems(item('title').not.gt('Hello'))
+    ).resolves.toMatchObject([first]);
+  });
+
+  it('should filter existing fields', async () => {
+    await repo.addItem({
+      title: null,
+      description: undefined,
+      kind: '',
+    });
+
+    await expect(
+      repo.searchItems(item('title').exist())
+    ).resolves.toMatchObject([third, second, first]);
+    await expect(
+      repo.searchItems(item('description').exist())
+    ).resolves.toMatchObject([third, second, first]);
+    await expect(repo.searchItems(item('kind').exist())).resolves.toMatchObject(
+      [
+        {
+          title: null,
+          kind: '',
+        },
+        third,
+        second,
+        first,
+      ]
+    );
+  });
+
+  it('should filter *not* existing fields', async () => {
+    const expectedNull = {
+      title: null,
+      kind: '',
+    };
+
+    await repo.addItem({
+      ...expectedNull,
+      description: undefined,
+    });
+
+    await expect(
+      repo.searchItems(item('title').not.exist())
+    ).resolves.toMatchObject([expectedNull]);
+    await expect(
+      repo.searchItems(item('description').not.exist())
+    ).resolves.toMatchObject([expectedNull]);
+    await expect(
+      repo.searchItems(item('kind').not.exist())
+    ).resolves.toMatchObject([]);
+  });
+
+  it('should filter null fields', async () => {
+    const expectedNull = {
+      title: null,
+      kind: '',
+    };
+
+    await repo.addItem({
+      ...expectedNull,
+      description: undefined,
+    });
+
+    await expect(
+      repo.searchItems(item('title').isNull())
+    ).resolves.toMatchObject([expectedNull]);
+    await expect(
+      repo.searchItems(item('description').isNull())
+    ).resolves.toMatchObject([]);
+    await expect(
+      repo.searchItems(item('kind').isNull())
+    ).resolves.toMatchObject([]);
+  });
+
+  it('should filter *not* null fields', async () => {
+    const expectedNull = {
+      title: null,
+      kind: '',
+    };
+
+    await repo.addItem({
+      ...expectedNull,
+      description: undefined,
+    });
+
+    await expect(
+      repo.searchItems(item('title').not.isNull())
+    ).resolves.toMatchObject([third, second, first]);
+    await expect(
+      repo.searchItems(item('description').not.isNull())
+    ).resolves.toMatchObject([expectedNull, third, second, first]);
+    await expect(
+      repo.searchItems(item('kind').not.isNull())
+    ).resolves.toMatchObject([expectedNull, third, second, first]);
+  });
+
+  it('should filter by equality', async () => {
+    await expect(
+      repo.searchItems(item('kind').eq('feature'))
+    ).resolves.toMatchObject([third]);
+
+    await expect(
+      repo.searchItems(item('kind').eq('issue'))
+    ).resolves.toMatchObject([second, first]);
   });
 
   it('should filter by difference', async () => {
-    expect(
+    await expect(
       repo.searchItems(item('kind').not.eq('feature'))
     ).resolves.toMatchObject([second, first]);
 
-    expect(
+    await expect(
       repo.searchItems(item('kind').not.eq('issue'))
     ).resolves.toMatchObject([third]);
 
-    expect(
+    await expect(
       repo.searchItems(item('kind').not.eq('feature'))
-    ).resolves.toMatchObject([third]);
-
-    expect(
-      repo.searchItems(item('kind').not.eq('issue'))
     ).resolves.toMatchObject([second, first]);
   });
 });
